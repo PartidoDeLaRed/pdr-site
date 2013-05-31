1b5fc9b19f08ed1c1597108df95e22987d4f8061
Cristian Douce <cristian@gravityonmars.com>
2013-05-31 10:52:50 -0300

    Update counter

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express')
   , path = require('path')
   , members = require('./members')
   , utils = require('./utils')
-  , counter = '766'; // default counter value
+  , counter = '730'; // default counter value
 
 var app = express();
 
